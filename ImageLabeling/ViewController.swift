04978e0f4e8ed0e22e2971bcf6eb45245547f1d4
Kanta'MacPro <mfireball@kantamacpros-macbook-pro.local>
2018-12-05 22:28:06 +0700

    Initial Commit

---
+++ ImageLabeling/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageLabeling
+//
+//  Created by Kanta'MacPro on 5/12/2561 BE.
+//  Copyright © 2561 Kanta'MacPro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
